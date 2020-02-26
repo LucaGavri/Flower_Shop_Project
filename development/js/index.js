1716eecd95267a3a94bc2d2e70a327cffbf53c61
Lukasz Gawrys <lukaszgawrys13@gmail.com>
2020-02-26 17:52:57 +0100

    slider

--- development/js/index.js
+++ development/js/index.js
@@ -38,3 +38,21 @@ burger.addEventListener('click', function (e) {
     }
 });
 
+//slider
+
+let myIndex = 0;
+carousel();
+
+function carousel() {
+    let i;
+    let x = document.getElementsByClassName("sliderText");
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    myIndex++;
+    if (myIndex > x.length) {
+        myIndex = 1
+    }
+    x[myIndex - 1].style.display = "block";
+    setTimeout(carousel, 6000);
+}
