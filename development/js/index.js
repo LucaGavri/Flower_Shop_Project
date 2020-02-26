320f2ff5c6bc3f747e3fe89747f35250b3a31f42
Lukasz Gawrys <lukaszgawrys13@gmail.com>
2020-02-26 21:50:52 +0100

    first rwd

--- development/js/index.js
+++ development/js/index.js
@@ -1,28 +1,3 @@
-//zmiejszanie headera po scrollu
-
-const header = document.querySelector("header");
-
-
-function widthForHeader(){
-    if(window.innerWidth > 750){
-        window.onscroll = function () {
-            headerScroll();
-        };
-        function headerScroll() {
-            if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-                header.style.height = "50px";
-            } else {
-                header.style.height = "80px";
-            }
-        }
-        headerScroll();
-    }
-    else {
-        header.style.height = "50px";
-    }
-}
-widthForHeader();
-
 //animacja i rozwijanie menu burgera
 const burger = document.querySelector('.burger');
 const navMobile = document.querySelector('.headerNavigationMobile');
